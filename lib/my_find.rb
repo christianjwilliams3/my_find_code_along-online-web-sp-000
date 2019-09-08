069d2644df3fd7d7d3eae5e01cd2d89f377324e6
christian williams <christian.williamsj@gmail.com>
2019-09-08 07:00:33 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,11 +1,12 @@
 require 'pry'
 def my_find(collection)
   i = 0 
-    while i <
-      collection.length 
-        if yield(collection[i])
-          return collection[i]
-        end 
-        i = i + 1 
-    end
+  while i < 100
+collection.length 
+  return
+  collection[i] if 
+yield(collection[i])
+    i = i + 1
+  end 
+  collection
 end
